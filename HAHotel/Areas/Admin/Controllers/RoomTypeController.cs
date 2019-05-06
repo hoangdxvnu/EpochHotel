2ac93849390c06291d91008517ded7d802c6cbd0
anhdt1995 <trung.anh.duong.1995@gmail.com>
2019-05-07 00:05:36 +0700

    Admin Initialization

---
+++ HAHotel/Areas/Admin/Controllers/RoomTypeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HAHotel.Areas.Admin.Controllers
+{
+    public class RoomTypeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
