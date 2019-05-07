15108638bc73914997856058d906c897b6f5b92b
Hoang Do <hoangdx@hotmail.com>
2019-05-08 01:47:14 +0700

    Menu management

---
+++ HAHotel/Repository/ISystemMenuRepository.cs
@@ -0,0 +1,12 @@
+﻿using HAHotel.Models;
+using System.Collections.Generic;
+
+namespace HAHotel.Repository
+{
+    public interface ISystemMenuRepository
+    {
+        List<SystemMenu> GetListRoomType(RoomTypeRequest request);
+
+        SystemMenu GetById(int id);
+    }
+}
---
+++ HAHotel/Repository/SystemMenuRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using HAHotel.Models;
+using HAHotel.Repository.Common;
+
+namespace HAHotel.Repository
+{
+    public class SystemMenuRepository: ISystemMenuRepository
+    {
+        private readonly IDatabase _database;
+
+        public SystemMenuRepository(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public List<SystemMenu> GetListRoomType(RoomTypeRequest request)
+        {
+            var param = new SqlServerParameter();
+            if (request.IsActive.HasValue)
+                param.Add_Parameter("@_IsActive", request.IsActive);
+            param.Add_Parameter("@_PageSize", request.PageSize);
+            param.Add_Parameter("@_PageIndex", request.PageIndex);
+
+            var data = _database.ExecuteToTable("SystemMenu_GetList", param, ExecuteTypeEnum.StoredProcedure);
+            if (data != null && data.Rows.Count > 0)
+            {
+                return SqlMapper<SystemMenu>.Map(data);
+            }
+
+            return new List<SystemMenu>();
+        }
+
+        public SystemMenu GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
