c008fb03e07531e2751c3649c77eb17807a0504f
anhdt1995 <trung.anh.duong.1995@gmail.com>
2019-05-08 00:21:12 +0700

    Room type

---
+++ HAHotel/Models/RoomTypeRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HAHotel.Models
+{
+    public class RoomTypeRequest
+    {
+        public bool? IsActive { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int PageIndex { get; set; }
+    }
+}
