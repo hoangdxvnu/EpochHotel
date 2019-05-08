a30ff37a793d2322a2614dd58777d3d339f13b43
anhdt1995 <trung.anh.duong.1995@gmail.com>
2019-05-08 23:57:47 +0700

    Delete

---
+++ HAHotel/AdminStyle/js/site.js
@@ -0,0 +1,9 @@
+﻿function confirmDelete(link) {
+    if (!link) {
+        return;
+    }
+    var conf = confirm("Bạn có chắc chắc muốn xóa?");
+    if (conf) {
+        window.location.replace(link.data("url"));
+    }
+}
