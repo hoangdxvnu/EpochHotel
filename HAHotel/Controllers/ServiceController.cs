417bc2cfff1a3b264526a299c69b48955bb17483
Hoang Do <hoangdx@hotmail.com>
2019-05-15 00:21:33 +0700

    Updating book

--- HAHotel/Controllers/ServiceController.cs
+++ HAHotel/Controllers/ServiceController.cs
@@ -1,6 +1,23 @@
-﻿namespace HAHotel.Controllers
+﻿using System.Web.Mvc;
+using HAHotel.Repository;
+
+namespace HAHotel.Controllers
 {
-    public class ServiceController
+    public class ServiceController: BaseController
     {
+        public ServiceController(ILayoutRepository layoutRepository) : base(layoutRepository)
+        {
+            
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = LayoutRepository.LoadFooterDefaul();
+            SetCurrentMenu();
+            ViewBag.ListRooms = LayoutRepository.FetchRoomTypes();
+
+            return View(model);
+        }
     }
 }
